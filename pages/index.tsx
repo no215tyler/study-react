ce1bf9d81cf42ae1184e9880165ff4cad3b29000
no215tyler <no.215.tyler@gmail.com>
2024-07-27 16:48:42 +0900

    ⚗️ クリックイベントのテスト

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import { Inter } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const foo: string = 'test';
+  const handleClick = useCallback((e: any) => {
+    console.log(`URL: ${e.target.href}`);
+    e.preventDefault();
+    alert(foo);
+  }, []);
   return (
     <>
       <Header />
+      <div className="flex justify-center">
+        <a href="/about" onClick={handleClick}>
+          ボタン
+        </a>
+      </div>
       <Main page="index" />
     </>
   );
