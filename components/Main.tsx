68c8c5d1d8e8305570ccdf20cb129dcefbd79b09
no215tyler <no.215.tyler@gmail.com>
2024-07-27 20:10:25 +0900

    ⚡️ useCallback, useEffectの第2引数の使い方

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,20 +1,10 @@
 import { Inter } from "next/font/google";
 import { Links } from "@/components/Links";
 import { Headline } from "@/components/Headline";
-import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export function Main(props: any) {
-  // マウント処理
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    
-    // アンマウント処理
-    return () => {
-      document.body.style.backgroundColor = "";      
-    }
-  }, []);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,30 @@
 import { Inter } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const[count, setCount] = useState<number>(1)
 
-  const handleClick = (e: any) => {
-    setCount((count: number) => count + 1);
-    setCount((count: number) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count: number) => count + 1);
+    }
+  }, [count]);
+
+  // マウント処理
+  useEffect(() => {
+    console.log(`マウント時: ${count}`);
+    document.body.style.backgroundColor = "lightblue";
+    
+    // アンマウント処理
+    return () => {
+      console.log(`アンマウント時: ${count}`);
+      document.body.style.backgroundColor = "";      
+    }
+  }, [count]); // , [count, foo, bar, hoge] この変数の値に変更が生じた場合にuseEffectの処理が走る
 
   return (
     <>
